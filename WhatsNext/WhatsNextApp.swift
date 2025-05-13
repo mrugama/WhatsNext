2a34b20af3c3197d976931fcece1076aa38658b0
Marlon Rugama <mrugama@gmail.com>
2025-05-13 10:57:17 -0400

    Initial Commit

---
+++ WhatsNext/WhatsNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNextApp.swift
+//  WhatsNext
+//
+//  Created by Marlon Rugama on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
